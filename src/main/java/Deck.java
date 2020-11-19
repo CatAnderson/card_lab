bb59a30120463506a67dd2f0025772f88193e303
Alan Parley <alanparley@hotmail.com>
2020-11-19 14:36:51 +0000

    can add 1 card to deck

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+
 public class Deck {
+
+    private ArrayList<Card> cards;
+
+    public Deck() {
+        this.cards = new ArrayList<>();
+    }
+
+    public int countCards(){
+        return this.cards.size();
+    }
+    
+    public void addCard(Card card){
+        this.cards.add(card);
+    }
 }
--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -1,2 +1,27 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DeckTest {
+
+    private Card card;
+    private Deck deck;
+
+    @Before
+    public void before(){
+        card = new Card(SuitType.DIAMONDS, RankType.KING);
+        deck = new Deck();
+    }
+
+    @Test
+    public void hasNoCards(){
+        assertEquals(0, deck.countCards());
+    }
+
+    @Test
+    public void hasOneCardInDeck(){
+        deck.addCard(card);
+        assertEquals(1, deck.countCards());
+    }
 }
